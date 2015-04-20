82a21a898217e1c4730bb0cb95a185f4d9d7889b
pantobp <nemanjapantovic@live.ac.me>
2015-04-20 14:37:35 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,42 @@
+<?php
+	$adresa[0]="331 Clematis St."; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="West Palm Beach, Fl 33401";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="West Palm Beach";
+	$telephone[0]=5618337755;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="125 Worth Avenue"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="Palm Beach, FL , Fl 33401";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Palm Beach";
+	$telephone[1]=5618330550;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="828 W Indiantown Road"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="Jupiter, Fl 33458";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Jupiter";
+	$telephone[2]=5617474449;// Broj koji se poziva na dugme CALL US
+
+	$adresa[3]="8221 Glades Road #E8"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[3]="Boca Raton, Fl 33434";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[3]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[3]="Boca Raton";
+	$telephone[3]=5614887737;// Broj koji se poziva na dugme CALL US
+
+	$adresa[4]="541 5th Avenue South"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[4]="Naples, Fl 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[4]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[4]="Naples I";
+	$telephone[4]=2396497737;// Broj koji se poziva na dugme CALL US
+
+	$adresa[5]="766 5th Ave. South"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[5]="Naples, Fl 34102";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[5]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[5]="Naples II";
+	$telephone[5]=2396497200;// Broj koji se poziva na dugme CALL US
+
+	$centratelephone=2396497737;
+	$companynumber=6;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="flip"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
